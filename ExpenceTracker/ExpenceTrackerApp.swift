be5d2b8168905208b80ae88ef312c30d1b341c19
aayushkumar20 <aayushkumarjsr16@protonmail.com>
2023-07-29 02:27:01 +0530

    Initial Commit

---
+++ ExpenceTracker/ExpenceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenceTrackerApp.swift
+//  ExpenceTracker
+//
+//  Created by Aayush kumar on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
